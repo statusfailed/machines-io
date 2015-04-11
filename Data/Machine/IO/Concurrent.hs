36e20995cb88c2b9feba924476a292f2dc4ef874
Paul Wilson <sf@statusfailed.com>
2015-04-11 18:27:30 +0100

    changed interface of toChan and runChan and updated cabal

--- Data/Machine/IO/Concurrent.hs
+++ Data/Machine/IO/Concurrent.hs
@@ -11,19 +11,19 @@ import Control.Monad.IO.Class
 import Control.Concurrent         hiding (yield)
 import Control.Concurrent.Chan
 
-toChan :: MonadIO m => Chan a -> MachineT m k a -> MachineT m k b
-toChan c s = s ~> repeatedly (await >>= liftIO . writeChan c)
+toChan :: MonadIO m => Chan a -> ProcessT m a b
+toChan c = repeatedly (await >>= liftIO . writeChan c)
 
 fromChan :: MonadIO m => Chan a -> SourceT m a
 fromChan c = repeatedly $ liftIO (readChan c) >>= yield
 
 runChan :: MonadIO m => (m () -> IO ()) -> Chan a -> SourceT m a -> IO ()
-runChan f c s = f . runT_ $ toChan c s
+runChan f c s = f . runT_ $ s ~> toChan c
 
 forkChan :: Foldable t => t (MachineT IO k a) -> IO (Chan a)
 forkChan xs = do
   c <- newChan
-  mapM_ (forkIO . runT_ . toChan c) xs
+  mapM_ (forkIO . runT_ . (~> toChan c)) xs
   return c
 
 mergeIO :: (MonadIO m, Foldable t) => t (MachineT IO k a) -> IO (MachineT m k' a)
